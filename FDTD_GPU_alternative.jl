076dc4490acb0e8bb7a7566821fae42e6f489cdd
spryt13 <39279125+spryt13@users.noreply.github.com>
2024-02-20 11:10:17 +0100

    FDTD for GPU
    A refined implementation with creation of an entire array in GPU memory for saving calculated data.

--- FDTD_GPU_alternative.jl
+++ FDTD_GPU_alternative.jl
@@ -2,46 +2,44 @@ using CUDA
 using BenchmarkTools
 using Plots
 
-size = 200;
-max_time = 200;
-number_of_threads = 256;
+max_len = 200;
+max_time = 1000;
+number_of_threads = 16;
 
 imp0::Float64 = 377.0;
-imp = 3*imp0;
-ez = CUDA.zeros(max_time, size);
-hy = CUDA.zeros(max_time, size-1);
+imp = 3imp0;
+ez = CUDA.zeros(max_time, max_len);
+hy = CUDA.zeros(max_time, max_len-1);
 
 function kernel_hy(hy, ez, imp0, time)
-    index = threadIdx().x
-    stride = blockDim().x
-    for i = index:stride:length(hy)
+    index = threadIdx().x;
+    stride = blockDim().x;
+    for i = index:stride:size(hy)[2]
         @inbounds hy[time, i] = hy[time-1, i] + (ez[time-1, i+1] - ez[time-1, i]) / imp0
     end
     return nothing
 end
 
 function kernel_ez(hy, ez, imp0, time)
-    index = threadIdx().x
-    stride = blockDim().x
-    #@cuprintln("thread $index")
-    for i = index:stride:length(ez)-2
-        @inbounds ez[time, i+1] = ez[time-1, i+1] + (hy[time, i+1] - hy[time, i]) * imp0
+    index = threadIdx().x;
+    stride = blockDim().x;
+    
+    for i = index:stride:size(ez)[2]
+        if i == 1
+            ez[time, i] = exp(-(time-30.) * (time-30.) / 100.)
+        elseif i == 200
+            ez[time, i] = ez[time-1, i] * (1 - imp0/3imp0) - hy[time, i-1] * 2imp0
+            ez[time, i] = ez[time, i] / (1 + imp0/3imp0)
+        else
+            @inbounds ez[time, i] = ez[time-1, i] + (hy[time, i] - hy[time, i-1]) * imp0
+        end
     end
-    return nothing
-end
-
-function border_ez(size, imp0, imp, time)
-    ez[time, size] = ez[time-1, size] * (1 - imp0/imp) - hy[time, size-1] * 2imp0
-    ez[time, size] = ez[time-1, size] / (1 + imp0/imp)
     return nothing
 end
 
 function calculate_rows(time)
     @cuda threads=number_of_threads kernel_hy(hy, ez, imp0, time)
     @cuda threads=number_of_threads kernel_ez(hy, ez, imp0, time)
-    ez[time, 1] = exp(-(time-30.) * (time-30.) / 100.)
-    #ez[1] += exp(-(time - 30.) * (time - 30.) / 100.);
-    border_ez(size, imp0, imp, time)
 end
 
 function FDTD()
