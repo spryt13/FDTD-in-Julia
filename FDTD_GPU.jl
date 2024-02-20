5cd467b09f89b506869c2100559487a732344c5c
spryt13 <39279125+spryt13@users.noreply.github.com>
2024-02-20 13:35:23 +0100

    FDTD for GPU
    A refined implementation of FDTD for GPU. This version calculates fields in vectors, which are then copied outside of GPU's memory.

--- FDTD_GPU.jl
+++ FDTD_GPU.jl
@@ -2,60 +2,62 @@ using CUDA
 using BenchmarkTools
 using Plots
 
-size = 200;
-number_of_threads = 16;
+max_len = 200;
 max_time = 1000;
+number_of_threads = 16;
+save_step = 50;
 
 imp0::Float64 = 377.0;
-imp = 3*imp0;
-ez = CUDA.zeros(size);
-hy = CUDA.zeros(size-1);
-series_ez = zeros(max_time, size);
-series_hy = zeros(max_time, size);
+imp = 3imp0;
+ez = CUDA.zeros(max_len);
+hy = CUDA.zeros(max_len-1);
+series_ez = zeros(div(max_time, save_step), max_len);
+series_hy = zeros(div(max_time, save_step), max_len-1);
 
-function kernel_hy(hy, ez, imp0) # obliczanie natężenia pola magnetycznego
+function kernel_hy(hy, ez, imp0)
     index = threadIdx().x
     stride = blockDim().x
     for i = index:stride:length(hy)
-        @inbounds hy[i] += (ez[i+1] - ez[i]) / imp0
+        @inbounds hy[i] = hy[i] + (ez[i+1] - ez[i]) / imp0
     end
     return nothing
 end
 
-function kernel_ez(hy, ez, imp0) # obliczanie natężenia pola elektrycznego
+function kernel_ez(hy, ez, imp0, imp, time)
     index = threadIdx().x
     stride = blockDim().x
-    for i = index:stride:length(ez)-1
-        @inbounds ez[i+1] += (hy[i+1] - hy[i]) * imp0
+    for i = index:stride:length(ez)
+        if i == 1
+            ez[i] = exp(-(time-30.) * (time-30.) / 100.)
+        elseif i == length(ez)
+            ez[i] = ez[i] * (1 - imp0/imp) - hy[i-1] * 2imp0
+            ez[i] = ez[i] / (1 + imp0/imp)
+        else
+            @inbounds ez[i] = ez[i] + (hy[i] - hy[i-1]) * imp0
+        end
     end
     return nothing
 end
 
-function border_ez(size, imp0, imp) # warunek brzegowy
-    ez[size] = ez[size] * (1 - imp0/imp) - hy[size-1] * 2imp0
-    ez[size] = ez[size] / (1 + imp0/imp)
-end
-
-function calculate_rows(time) # obliczenie wszystkiego dla danej chwili
+function calculate_rows(time)
     @cuda threads=number_of_threads kernel_hy(hy, ez, imp0)
-    @cuda threads=number_of_threads kernel_ez(hy, ez, imp0)
-    ez[1] = exp(-(time-30.) * (time-30.) / 100.) # źródło pola elektrycznego
-    border_ez(size, imp0, imp)
+    @cuda threads=number_of_threads kernel_ez(hy, ez, imp0, imp, time)
 end
 
-function save_FDTD(time) # zapis wartości pól
-    for count = 1:size
-        series_ez[time, count] = ez[count]
-    end
-    for count = 1:size-1
-        series_hy[time, count] = hy[count]
-    end
+function save_FDTD(time)
+    series_ez[time, :] = ez
+    series_hy[time, :] = hy
 end
 
-function FDTD() # wykomentowanie save_FDTD() wyłącza zapis
+function FDTD()
+    count = 1;
     for time = 1:max_time
         calculate_rows(time)
-	save_FDTD()
+        i = divrem(count, save_step)
+        if i[2] == 0
+            save_FDTD(i[1])
+        end
+        count += 1;
     end
 end
 
