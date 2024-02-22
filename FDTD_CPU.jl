828aca7df931e13ff7ea91dcff2edef4092d1c11
spryt13 <39279125+spryt13@users.noreply.github.com>
2024-02-22 15:01:35 +0100

    Comments added to the code
    Added comments in English language

--- FDTD_CPU.jl
+++ FDTD_CPU.jl
@@ -1,44 +1,44 @@
 using BenchmarkTools
 using Plots
 
-# rozmiar obliczanej przestrzeni
+# size (length) of the object and number of timesteps
 size = 200;
 max_time = 1000;
 
 imp0::Float64 = 377.0;
 imp = 3*imp0;
-# wektory do wykonywania obliczeń
+# vectors for calculations
 ez = zeros(size);
 hy = zeros(size-1);
-# macierze do zapisywania wyników
+# arrays for saving results in
 series_ez = zeros(max_time, size);
 series_hy = zeros(max_time, size);
 
-function count_hy(count, imp0) # oblicz natężenie pola magnetycznego
+function count_hy(count, imp0) # calculation of electric field
     hy[count] = hy[count] + (ez[count+1] - ez[count]) / imp0
 end
 
-function count_ez(count, imp0) # oblicz natężenie pola magnetycznego
+function count_ez(count, imp0) # calculation of magnetic field
     ez[count] = ez[count] + (hy[count] - hy[count-1]) * imp0
 end
 
-function border_ez(size, imp0, imp) # punkt brzegowy
+function border_ez(size, imp0, imp) # boundary point
     ez[size] = ez[size] * (1 - imp0/imp) - hy[size-1] * 2imp0
     ez[size] = ez[size] / (1 + imp0/imp)
 end
 
-function calculate_rows(time) # iteracja wszystkiego w danej chwili czasowej
+function calculate_rows(time) # iteration in a given time
     for count = 1:size-1
         count_hy(count, imp0)
     end
     for count = 2:size-1
         count_ez(count, imp0)
     end
-    ez[1] = exp(-(time-30.) * (time-30.) / 100.) # źródło pola elektrycznego
+    ez[1] = exp(-(time-30.) * (time-30.) / 100.) # electric field source (boundary point)
     border_ez(size, imp0, imp)
 end
 
-function save_FDTD(time) # zapis wyników FDTD
+function save_FDTD(time) # save FDTD results
     for count = 1:size
         series_ez[time, count] = ez[count]
     end
@@ -47,8 +47,8 @@ function save_FDTD(time) # zapis wyników FDTD
     end
 end
 
-function FDTD()                    # wywołanie FDTD. Wykomentowanie save_FDTD() spowoduje brak zapisu
-    for time = 1:max_time          # i zapamiętanie jedynie ostatniej chwili czasowej w wektorach ez i hy.
+function FDTD()
+    for time = 1:max_time          
         calculate_rows(time)
         save_FDTD(time)
     end
