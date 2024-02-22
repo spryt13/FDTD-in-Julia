e251a4875f24335e65a567768cbca1860ca2f280
spryt13 <39279125+spryt13@users.noreply.github.com>
2024-02-22 15:13:27 +0100

    Comments added to the code
    Added comments in English language.

--- FDTD_GPU_alternative.jl
+++ FDTD_GPU_alternative.jl
@@ -2,16 +2,18 @@ using CUDA
 using BenchmarkTools
 using Plots
 
-max_len = 200;
-max_time = 1000;
-number_of_threads = 16;
+max_len = 200; # length of the environment
+max_time = 1000; # number of timesteps
+number_of_threads = 16; number of GPU threads
 
 imp0::Float64 = 377.0;
 imp = 3imp0;
-ez = CUDA.zeros(max_time, max_len);
+ez = CUDA.zeros(max_time, max_len); 
 hy = CUDA.zeros(max_time, max_len-1);
+# Please note that there is no separate array for saving data and both ez and hy entities are arrays instead of vectors.
+# All the data calculated is saved and kept within GPU memory.
 
-function kernel_hy(hy, ez, imp0, time)
+function kernel_hy(hy, ez, imp0, time) # calculate magnetic field
     index = threadIdx().x;
     stride = blockDim().x;
     for i = index:stride:size(hy)[2]
@@ -20,14 +22,13 @@ function kernel_hy(hy, ez, imp0, time)
     return nothing
 end
 
-function kernel_ez(hy, ez, imp0, imp, time)
+function kernel_ez(hy, ez, imp0, imp, time) # calculate electric field
     index = threadIdx().x;
     stride = blockDim().x;
-    
     for i = index:stride:size(ez)[2]
-        if i == 1
-            ez[time, i] = exp(-(time-30.) * (time-30.) / 100.)
-        elseif i == size(ez)[2]
+        if i == 1 # boundary point
+            ez[time, i] = exp(-(time-30.) * (time-30.) / 100.) # source of electric field
+        elseif i == size(ez)[2] # boundary point
             ez[time, i] = ez[time-1, i] * (1 - imp0/imp) - hy[time, i-1] * 2imp0
             ez[time, i] = ez[time, i] / (1 + imp0/imp)
         else
@@ -37,7 +38,7 @@ function kernel_ez(hy, ez, imp0, imp, time)
     return nothing
 end
 
-function calculate_rows(time)
+function calculate_rows(time) # calculate FDTD for a given time moment
     @cuda threads=number_of_threads kernel_hy(hy, ez, imp0, time)
     @cuda threads=number_of_threads kernel_ez(hy, ez, imp0, imp, time)
 end
